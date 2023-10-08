8f9128cad472882a8dd6d1ccc668e09975fd5cb5
tct123 <42028373+tct123@users.noreply.github.com>
2023-10-08 23:32:35 +0200

    Update app.py

--- src/togaquotes/app.py
+++ src/togaquotes/app.py
@@ -8,7 +8,6 @@ import quotesgeneratorapi_wrapper
 
 
 class TogaQuotes(toga.App):
-
     def startup(self):
         """
         Construct and show the Toga application.
@@ -18,28 +17,35 @@ class TogaQuotes(toga.App):
         show the main window.
         """
         main_box = toga.Box()
+        main_box.style.direction = "column"
         try:
-            self.qoutegen = quotesgeneratorapi_wrapper.getQuotes(category="happiness", api_key = "As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk")
+            self.qoutegen = quotesgeneratorapi_wrapper.getQuotes(
+                category="happiness", api_key="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk"
+            )
             content = self.qoutegen
             content, quoteauthor = content.split(sep="\n\n")
         except:
             content = "Check your Connection"
             author = ""
-        self.quote = toga.Label(text=content)
-        self.quoteauthor = toga.Label(text=quoteauthor)
-        refreshbtn = toga.Button(text="Refresh",on_press=self.get)
+        self.quote = toga.Label(text=content, style=Pack(padding=5))
+        self.quoteauthor = toga.Label(text=quoteauthor, style=Pack(padding=5))
+        refreshbtn = toga.Button(
+            text="Refresh", on_press=self.get, style=Pack(padding=5)
+        )
         main_box.add(self.quote)
         main_box.add(self.quoteauthor)
         main_box.add(refreshbtn)
-        
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
-    def get(self,widget):
+
+    def get(self, widget):
         self.quote.clear()
         try:
-            self.qoutegen = quotesgeneratorapi_wrapper.getQuotes(category="happiness", api_key = "As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk")
+            self.qoutegen = quotesgeneratorapi_wrapper.getQuotes(
+                category="happiness", api_key="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk"
+            )
             content = self.qoutegen
             content, quoteauthor = content.split(sep="\n\n")
         except:
@@ -47,7 +53,6 @@ class TogaQuotes(toga.App):
             author = ""
         self.quote.text = content
         self.quoteauthor.text
-
 
 
 def main():
